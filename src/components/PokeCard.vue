f866941c3dcf911d48553c171b6c9ea24e45e8dc
tthheusalmeida <tthheusalmeida@gmail.com>
2021-04-25 09:26:52 -0300

    styles: set pokecard styles

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -11,11 +11,11 @@
     </div>
 
     <div class="card_id">
-      <p>n°{{ id }}</p>
+      n°{{ id }}
     </div>
 
     <div class="card_name">
-      <p>{{ name }}</p>
+      {{ name }}
     </div>
 
     <div class="card_types">
@@ -73,6 +73,11 @@ export default {
 
 <style lang="scss">
   .card {
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+
+    font-family: 'Acme', arial;
     font-size: 0.8rem;
     text-transform: capitalize;
     padding: 10px;
@@ -81,12 +86,10 @@ export default {
     border-radius: 8px;
     box-shadow: var(--card-black) 0px 10px 15px -5px;
 
+    cursor: pointer;
     transition: all 0.2s ease;
 
     &_img {
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
       background: var(--card-gray);
       border-radius: 100%;
       min-width: 130px;
@@ -96,46 +99,33 @@ export default {
       }
     }
 
-    &_id {
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      margin-top: 8px;
+    &_id, &_name {
+      text-align: center;
+      align-self: center;
+    }
 
-      p {
-        padding: 1px 6px;
-        margin: 0px;
-        border-radius: 8px;
-        background: var(--card-gray);
-      }
+    &_id {
+      margin-top: 5px;
+      width: 50px;
+      padding: 1px 6px;
+      border-radius: 8px;
+      background: var(--card-gray);
     }
 
     &_name {
-      font-family: 'Acme', arial;
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      margin-top: 8px;
-
-      p {
-        font-size: 16px;
-        font-weight: bold;
-        padding-bottom: 4px;
-        margin: 0;
-      }
+      font-size: 16px;
+      font-weight: bold;
+      margin: 3px 0;
     }
 
     &_types {
       display: grid;
       grid-template-columns: repeat(2, 1fr);
       gap: calc(5vmin + 2px);
-      margin-top: 4px;
 
       .type {
         text-align: center;
         display: inline;
-        color: white;
-        background: magenta;
         border-radius: 5px;
         padding: 2px 5px;
       }
