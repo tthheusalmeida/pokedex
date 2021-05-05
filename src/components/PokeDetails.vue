6f6d390fcef5ee430739d2d1d0dfbab84b19d126
tthheusalmeida <tthheusalmeida@gmail.com>
2021-05-05 12:03:41 -0300

    chore: add slice to pokeDetails

--- src/components/PokeDetails.vue
+++ src/components/PokeDetails.vue
@@ -5,7 +5,7 @@
     >
       <v-spacer></v-spacer>
       <v-btn
-        elevation="0"
+        elevation="5"
         color="red darken-4"
         class="ma-2 white--text"
         @click="closePokemonDetails"
@@ -48,29 +48,47 @@
     </div>
 
     <div class="details_info">
-      <div
-        v-if="toogleInfo"
-        class="details_info_attributes"
-      >
-        <PolarChart
-          :categories="attributeCategories"
-          :series="attributeSeries"
-        />
+      <div class="details_info_sound">
+        <v-btn
+          icon
+          large
+          disabled
+          elevation="10"
+          class="grey darken-4"
+        >
+        </v-btn>
       </div>
 
-      <div
-        v-else
-        class="details_info_stats"
-      >
-        <PolarChart
-          :categories="statsCategories"
-          :series="statsSeries"
-        />
+      <div>
+        <div class="details_info_slices">
+          <div class="details_info_slices_one"></div>
+          <div class="details_info_slices_two"></div>
+        </div>
+
+        <div
+          v-if="toogleInfo"
+          class="details_info_attributes"
+        >
+          <PolarChart
+            :categories="attributeCategories"
+            :series="attributeSeries"
+          />
+        </div>
+
+        <div
+          v-else
+          class="details_info_stats"
+        >
+          <PolarChart
+            :categories="statsCategories"
+            :series="statsSeries"
+          />
+        </div>
       </div>
 
       <div class="details_info_buttons">
         <v-btn
-          elevation="1"
+          elevation="10"
           class="grey darken-4"
           @click="toggleDetailsInfo"
         >
@@ -80,7 +98,7 @@
         </v-btn>
 
         <v-btn
-          elevation="1"
+          elevation="10"
           class="grey darken-4"
           @click="toggleDetailsInfo"
         >
@@ -239,10 +257,37 @@ export default {
       display: flex;
       flex-direction: row;
 
+      &_slices {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin-bottom: 40px;
+
+        &_one,
+        &_two {
+          width: 150px;
+          padding: 8px 10px;
+          border-radius: 12px;
+          box-shadow: var(--card-black) 0px 2px 7px -2px;
+        }
+
+        &_one {
+          background: var(--darker-ruby-red);
+        }
+
+        &_two {
+          background: var(--card-dark-blue);
+        }
+      }
+
       &_stats,
       &_attributes {
         border: 5px solid var(--background-grass-type);
         border-radius: 5px;
+      }
+
+      &_sound {
+        margin: 10px;
       }
 
       &_stats{
