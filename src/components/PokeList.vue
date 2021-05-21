aff0b4f9b603df70def5261b614c5c15e4c351be
tthheusalmeida <tthheusalmeida@gmail.com>
2021-05-21 18:03:36 -0300

    feat: add search field

--- src/components/PokeList.vue
+++ src/components/PokeList.vue
@@ -1,21 +1,44 @@
 <template>
-  <div>
-    <div
-      v-if="getRequestStatus"
-      class="list"
-    >
-      <div class="pokemon" v-for="(pokemon, index) in getPokemons" :key="index">
-        <PokeCard
-          :id="getPokemonId(pokemon)"
-          :name="getPokemonName(pokemon)"
-          :types="getPokemonTypes(pokemon)"
-          :img="getPokemonImg(pokemon)"
-        />
-      </div>
-    </div>
+  <v-row no-gutters>
+    <v-col v-if="getRequestStatus">
+      <v-row class="pr-4">
+        <v-col no-gutters>
+          <div class="d-flex justify-center">
+            <div class="search-field">
+              <v-text-field
+                solo
+                dense
+                clearable
+                hide-details
+                color="grey lighten-1"
+                class="mt-2"
+                v-model="searchPokemon"
+                :label="searchPokemonLabel"
+              />
+            </div>
+          </div>
+          <v-row
+            no-gutters
+            class="pokemons d-flex flex-wrap justify-center mt-3"
+          >
+            <div
+              v-for="(pokemon, index) in pokemons" :key="index"
+            >
+              <PokeCard
+                :id="getPokemonId(pokemon)"
+                :name="getPokemonName(pokemon)"
+                :types="getPokemonTypes(pokemon)"
+                :img="getPokemonImg(pokemon)"
+                class="ma-2"
+              />
+            </div>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-col>
 
     <Loading v-else />
-  </div>
+  </v-row>
 </template>
 
 <script>
@@ -31,6 +54,12 @@ export default {
     PokeCard,
     Loading,
   },
+  data() {
+    return {
+      searchPokemonLabel: 'Type the Pokémon name or id',
+      searchPokemon: '',
+    };
+  },
   computed: {
     ...mapGetters('pokemon', [
       'getPokemons',
@@ -38,6 +67,21 @@ export default {
     ]),
     requestStatus() {
       return this.getRequestStatus;
+    },
+    pokemons() {
+      if (this.searchPokemon) {
+        const isNumber = Number(this.searchPokemon);
+
+        if (isNumber) {
+          return this.getPokemons
+            .filter(({ id }) => id.toString().includes(this.searchPokemon.toString()));
+        }
+
+        return this.getPokemons
+          .filter(({ name }) => name.toLowerCase().includes(this.searchPokemon.toLowerCase()));
+      }
+
+      return this.getPokemons;
     },
   },
   methods: {
@@ -59,15 +103,7 @@ export default {
 </script>
 
 <style lang="scss">
-  .list {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    padding-right: 15px;
-  }
-
-  .pokemon {
-    margin: 10px 5px;
+  .search-field {
+    width: 260px;
   }
 </style>
