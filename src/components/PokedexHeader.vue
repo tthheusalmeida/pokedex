599f70e1b7e21d11be1ad530ede1d9aa2eeeeb89
tthheusalmeida <tthheusalmeida@gmail.com>
2021-05-21 18:13:36 -0300

    chore: removes media and maintains its values

--- src/components/PokedexHeader.vue
+++ src/components/PokedexHeader.vue
@@ -18,14 +18,10 @@ export default {
 
 <style lang="scss">
   .pokedex {
-    height: 210px;
+    height: 150px;
     width: 100vw;
     padding: 0;
     background: var(--darkest-ruby-red);
-
-    @media screen and (max-width: 800px) {
-      height: 150px;
-    }
 
     &_big-ball {
       padding: 0;
@@ -36,85 +32,56 @@ export default {
     }
 
     &_big-ball-outer {
-      height: 170px;
-      width: 170px;
+      height: 100px;
+      width: 100px;
       position: absolute;
       top: 25px;
       left: 40px;
       border-radius: 100%;
       background: var(--card-white);
 
-      @media screen and (max-width: 800px) {
-        height: 100px;
-        width: 100px;
-      }
-
       &_small-ball-red,
       &_small-ball-yellow,
       &_small-ball-green {
-        height: 40px;
-        width: 40px;
+        height: 30px;
+        width: 30px;
         border-radius: 100%;
         box-shadow: var(--darker-ruby-red) 0px -10px 10px 1px inset,
                     var(--lighter-ruby-red) 0px 10px 35px 5px inset;
-
-        @media screen and (max-width: 800px) {
-          height: 30px;
-          width: 30px;
-        }
       }
 
       &_small-ball-red {
         position: absolute;
-        left: 200px;
+        left: 140px;
         box-shadow: var(--darker-ruby-red) 0px -10px 10px 1px inset,
                     var(--lighter-ruby-red) 0px 10px 35px 5px inset;
-
-        @media screen and (max-width: 800px) {
-          left: 140px;
-        }
       }
 
       &_small-ball-yellow {
         position: absolute;
-        left: 300px;
+        left: 210px;
         box-shadow: var(--background-rock-type) 0px -10px 10px 1px inset,
                     var(--background-electric-type) 0px 10px 35px 5px inset;
-
-        @media screen and (max-width: 800px) {
-          left: 210px;
-        }
       }
 
       &_small-ball-green {
         position: absolute;
-        left: 400px;
+        left: 280px;
         box-shadow: var(--background-bug-type) 0px -10px 10px 1px inset,
                     var(--background-grass-type) 0px 10px 35px 5px inset;
-
-        @media screen and (max-width: 800px) {
-          left: 280px;
-        }
       }
     }
 
     &_big-ball-inner {
-      height: 150px;
-      width: 150px;
+      height: 80px;
+      width: 80px;
       position: absolute;
       top: 10px;
       left: 10px;
       border-radius: 100%;
       background: var(--background-ice-type-2);
-      box-shadow: var(--pokelogo-blue) 0px -80px 60px 10px inset,
-                var(--pokelogo-blue) 0px 8px 20px 10px inset;
-
-      @media screen and (max-width: 800px) {
-        height: 80px;
-        width: 80px;
-        box-shadow: var(--pokelogo-blue) 0px -40px 30px 10px inset,
+      box-shadow: var(--pokelogo-blue) 0px -40px 30px 10px inset,
                 var(--pokelogo-blue) 0px 4px 10px 10px inset;
-      }
     }
   }
 
